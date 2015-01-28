b37d8abe9fddcd76b4dca500af76a5ae77a1aeeb
Bryan Miller <bmiller1008@gmail.com>
2015-01-28 13:12:51 -0500

    Added the ability to compile using cmake system. Major modifications to the scripts/version.sh script. Now tests that GIT exists and that the script is being called from the root project directory (must contain the path .git), otherwise the version will be create as "UNKNOWN". OYAJSon/OYAJSon_version.cpp is no longer tracked as it is created before compile time by both the included cmake script and Code::Blocks project.

--- OYAJSon/OYAJSon_version.cpp
+++
@@ -1,3 +0,0 @@
-/* This is an automatically generated file. Do not modify! */
-const char* OYAJSON_VERSION = "v0.3 - 65ca099";
-
--- scripts/version.sh
+++ scripts/version.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
-SOURCE_PATH="OYAJSon/"
+
+BASE_PATH="`pwd`"
+if ! [ -z "$1" ]; then
+  BASE_PATH="$1"
+fi
+SOURCE_PATH="/OYAJSon/"
+
 VERSION_SOURCE_FILENAME="OYAJSon_version.cpp"
 
-rm ${SOURCE_PATH}${VERSION_SOURCE_FILENAME}
-echo "/* This is an automatically generated file. Do not modify! */
-const char* OYAJSON_VERSION = \"`git describe` - `git rev-parse --short HEAD`\";
-" >> ${SOURCE_PATH}${VERSION_SOURCE_FILENAME}
+create_version_source() {
+  if ! [ -d ${BASE_PATH}${SOURCE_PATH} ]; then
+    >&2 echo "Cannot find expected source path \"${BASE_PATH}${SOURCE_PATH}\"."
+    exit 1
+  fi
+
+  FILE_PATH=${BASE_PATH}${SOURCE_PATH}${VERSION_SOURCE_FILENAME}
+  if [ -f ${FILE_PATH} ]; then
+    rm ${FILE_PATH}
+  fi
+
+  VERSION="UNKNOWN"
+  if command -v git &>/dev/null && [ -d ${BASE_PATH}/.git ]; then
+    VERSION="`git describe`"
+  else
+    echo "Either GIT is not installed, or \"${BASE_PATH}/.git\" does not exist."
+  fi
+
+  echo "/* This is an automatically generated file. Do not modify! */" >> ${FILE_PATH}
+  echo "const char* OYAJSON_VERSION = \"${VERSION}\";" >> ${FILE_PATH}
+  echo "" >> ${FILE_PATH}
+
+  echo "FILE ${FILE_PATH} GENERATED SUCCESSFULLY"
+}
+
+
+create_version_source
